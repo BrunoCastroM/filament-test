b25001cd297c9b5b18a58dcd519c334c9859b579
BrunoCastroM <brunocastromoura@gmail.com>
2025-02-07 16:56:10 +0000

    feat: #7 linking countries to states and states to cities

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -4,9 +4,13 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\EmployeeResource\Pages;
 use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\City;
 use App\Models\Employee;
+use App\Models\State;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Notifications\Collection;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -31,16 +35,29 @@ class EmployeeResource extends Resource
                     ->relationship(name: 'country', titleAttribute: 'name')
                     ->searchable()
                     ->preload()
+                    ->live()
                     ->required(),
                     Forms\Components\Select::make('state_id')
-                    ->relationship(name: 'state', titleAttribute: 'name')
+                    ->options(fn (Get $get) => $get('country_id') 
+                        ? State::query()
+                            ->where('country_id', $get('country_id'))
+                            ->pluck('name', 'id')
+                            ->toArray()
+                        : [])
                     ->searchable()
                     ->preload()
+                    ->live()
                     ->required(),
                     Forms\Components\Select::make('city_id')
-                    ->relationship(name: 'city', titleAttribute: 'name')
+                    ->options(fn (Get $get) => $get('state_id') 
+                        ? City::query()
+                            ->where('state_id', $get('state_id'))
+                            ->pluck('name', 'id')
+                            ->toArray()
+                        : [])
                     ->searchable()
                     ->preload()
+                    ->live()
                     ->required(),
                     Forms\Components\Select::make('department_id')
                     ->relationship(name: 'department', titleAttribute: 'name')
