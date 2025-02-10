cdcde499ec606cc42a035157c60e471cac0f45cb
BrunoCastroM <brunocastromoura@gmail.com>
2025-02-10 15:02:08 +0000

    feat: #16 add relations and slug to the team

--- app/Filament/Pages/Tenancy/RegisterTeam.php
+++ app/Filament/Pages/Tenancy/RegisterTeam.php
@@ -1,20 +1,20 @@
 <?php
 
 namespace App\Filament\Pages\Tenancy;
-
+ 
 use App\Models\Team;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Pages\Tenancy\RegisterTenant;
 use Illuminate\Support\Facades\Auth;
-
+ 
 class RegisterTeam extends RegisterTenant
 {
     public static function getLabel(): string
     {
         return 'Register team';
     }
-
+ 
     public function form(Form $form): Form
     {
         return $form
@@ -23,13 +23,13 @@ class RegisterTeam extends RegisterTenant
                 TextInput::make('slug'),
             ]);
     }
-
+ 
     protected function handleRegistration(array $data): Team
     {
         $team = Team::create($data);
-
+ 
         $team->members()->attach(Auth::user());
-
+ 
         return $team;
     }
 }
