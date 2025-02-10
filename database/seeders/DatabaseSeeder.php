c68ad20e71050db4f473718eb21ee26a4cb54d3b
BrunoCastroM <brunocastromoura@gmail.com>
2025-02-10 15:29:00 +0000

    feat: #16 filament multi-tenancy done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,15 +16,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('123456'),
+        ]);
 
         $this->call([
             CountrySeeder::class,
             StateSeeder::class,
             CitySeeder::class,
         ]);
+
+        Department::create(['name' => 'RH']);
     }
 }
